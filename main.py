e28890b5cddd3cd5a1c9b7701100d91bfb5ec226
Shahzod <97654383+shahzodjabbarov@users.noreply.github.com>
2025-04-28 09:23:19 +0900

    CREATE A FILE

---
+++ main.py
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+MY PROMPT:
+Hi. I want to make a tkinter app that will help with studying to students. 
+
+here's how the pages work.
+
+it will be 600height 800width
+
+PAGE 1: 
+
+{ it is a main page in the application, that will be opened when program starts . it will have 3 buttons. 
+
+2 rather bigger buttons on the center. and one smaller button on bottom left corner. 
+
+1 one of the big buttons on the left - REVIEW (goes to [page 2]).
+
+2nd big button on the right - NEW WORDS (goes to [page 3]).
+
+3rd small button on bottom left corner - HISTORY (goes to [page 5])
+
+}
+
+PAGE 2: 
+
+{ page for learning new words. it will also have 4 buttons. 
+
+1 normal size button on center and 2 buttons under it. and one smaller button on bottom left corner. 
+
+button on center - UPLOAD (lets user upload a file)
+
+name of the fill will be written on top of the button after being chosen. 
+
+1 one of the buttons under upload - QUIZ (goes to [page 4])
+
+2nd button under upload - NOTES (goes to [page 4])
+
+3rd small button on bottom left corner - BACK (goes to [page 1])
+
+}
+
+PAGE 3: 
+
+{ page for review function. it will also have 3 buttons. 
+
+2 rather big buttons on the center. and one smaller button on bottom left corner. 
+
+1 one of the big buttons - MAKE STORY (goes to [page 4])
+
+2nd big button - MNEMONICS (goes to [page 4])
+
+3rd small button on bottom left corner - BACK (goes to [page 1])
+
+}
+
+PAGE 4:
+
+{page for showing the results of the functions. 
+
+}'''
